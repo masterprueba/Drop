5e998b978711395ab1e37b23e5cf25b6fd81f581
Parakee15 <yoimar--007@hotmail.com>
2017-02-19 14:29:22 -0500

    Corrigiendo errores  v3  Modulos usuarios funcionando

--- src/Model/Login_Model.java
+++ src/Model/Login_Model.java
@@ -34,7 +34,7 @@ public class Login_Model<L> extends General_model {
         if (Via == 2) {
             query = "from TLogin where tlogUserLogin ='" + usuario.getTlogUserLogin() + "' and TDatosBasicosPersona.tdbpCedula <> " + usuario.getTDatosBasicosPersona().getTdbpCedula();
         } else if (Via == 3) {
-            query = "from TLogin where TDatosBasicosPersona.TdbpId ='" + usuario.getTDatosBasicosPersona().getTdbpId()+ "'";
+            query = "from TLogin where TDatosBasicosPersona.tdbpId ='" + usuario.getTDatosBasicosPersona().getTdbpId()+ "'";
         }
         TLogin result = (TLogin) s.createQuery(query).uniqueResult();
         s.getTransaction().commit();
