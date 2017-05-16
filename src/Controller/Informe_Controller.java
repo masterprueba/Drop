346fd18900f143fe5ac3e2c6125f3758f9e619ce
Parakee15 <yoimar--007@hotmail.com>
2017-05-15 22:51:24 -0500

    Bitacora cliente a un pelo

--- src/Controller/Informe_Controller.java
+++ src/Controller/Informe_Controller.java
@@ -5,7 +5,10 @@
  */
 package Controller;
 
+import Entity.TCuota;
 import Model.Prestamo_model;
+import java.util.Iterator;
+import java.util.List;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -19,7 +22,23 @@ public class Informe_Controller extends Controllers{
         DefaultTableModel tmodelop = (DefaultTableModel) prestamos.getModel();
         DefaultTableModel tmodelog = (DefaultTableModel) gastos.getModel();
         Prestamo_model modelo = new Prestamo_model();
-        
+        List<Object> prueba = modelo.informePrestamo();
+        Iterator itr = prueba.iterator();
+        Object[] f = new Object[11];
+        int i = 1;
+        while(itr.hasNext()){
+            f[0] = i;
+            Object[] obj = (Object[]) itr.next();
+            f[1] = obj[0];
+            f[2] = obj[1];
+            f[3] = obj[2];
+            f[4] = obj[3];
+            f[5] = obj[4];
+            f[6] = Integer.parseInt(String.valueOf(obj[4])) - Integer.parseInt(String.valueOf(obj[2]));
+            i++;
+            tmodelop.addRow(f);
+         }
+       prestamos.setModel(tmodelop);
     }
     
 }
--- src/Model/Prestamo_model.java
+++ src/Model/Prestamo_model.java
@@ -13,18 +13,18 @@ import java.util.List;
  *
  * @author Breiner
  */
-public class Prestamo_model extends Models{
+public class Prestamo_model<T> extends Models{
 
     //otra prueba andres
     
     public Prestamo_model() {
     }
     
-    public List<String> ConsultarUsuarioContraseña() {
+    public List<T> informePrestamo() {
         s = hibernateUtil.getSessionFactory();
         s.beginTransaction();
-        String query = "from TLogin where tlogUserLogin = and tlogPassword = '";
-        List<String> result = s.createQuery(query).list();
+        String query = "SELECT  DATE_FORMAT(cuota.TPrestamo.tpreFechaEntrega,'%y-%m-%d') as fecha ,cuota.TPrestamo.TPersona.TDatosBasicosPersona.tdbpCedula as cedula, (SELECT SUM(prestamo.tpreValorPrestamo) FROM TPrestamo as prestamo WHERE prestamo.TPersona.tperId = cuota.TPrestamo.TPersona.tperId) as prestado,  (SELECT SUM(prestamo.tpreValorTotal) FROM TPrestamo as prestamo WHERE prestamo.TPersona.tperId = cuota.TPrestamo.TPersona.tperId) as invertido, Sum(cuota.tcuoAbono) as pagado FROM TCuota as cuota";
+        List<T> result = s.createQuery(query).list();
         s.getTransaction().commit();
 
         return result;
