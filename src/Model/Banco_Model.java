71b6bce1ccac04fd61001c023867ec66e3e48c21
Parakee15 <yoimar--007@hotmail.com>
2018-01-25 20:53:39 -0500

    funciones de andres

--- src/Model/Banco_Model.java
+++ src/Model/Banco_Model.java
@@ -22,11 +22,55 @@ public class Banco_Model extends Models {
 
         s = hibernateUtil.getSessionFactory();
         s.beginTransaction();
-        String query = "FROM TBanco WHERE tbanTipo = '" + tipoBanco +"'";
+        String query = "FROM TBanco WHERE tbanTipo = '" + tipoBanco + "'";
         List<TBanco> result = s.createQuery(query).list();
         s.getTransaction().commit();
 
         return result;
     }
 
+    public long getSaldoDebitoCredito(String fechaIni, String fechaFin, char tipo) {
+        s = hibernateUtil.getSessionFactory();
+        s.beginTransaction();
+        String query = "FROM "
+                + "TMovimientoBanco as tmb, "
+                + "TBanco as tb "
+                + "WHERE tb.tbanCuenta= tmb.TBanco.tbanCuenta "
+                + "AND tb.tbanTipo = '" + tipo + "' "
+                + "AND tmb.tmovFecha BETWEEN  DATE(DATE_FORMAT('" + fechaIni + "', '%Y-%m-%d')) AND DATE(DATE_FORMAT('" + fechaFin + "', '%Y-%m-%d'))";
+        List<T> result = s.createQuery(query).list();
+        s.getTransaction().commit();
+
+        Iterator itr = result.iterator();
+        long valor = 0;
+        while (itr.hasNext()) {
+            Object[] obj = (Object[]) itr.next();
+            TMovimientoBanco temp = (TMovimientoBanco) obj[0];
+
+            if (tipo == 'C') {
+                valor += (((temp.getTmovSaldo() * temp.getTmovPorcentaje()) / 100) + temp.getTmovSaldo());
+            } else {
+                valor += temp.getTmovSaldo();
+            }
+
+        }
+        return valor;
+    }
+
+    public long getSaldoCuentas(String fechaIni, String fechaFin) {
+        s = hibernateUtil.getSessionFactory();
+        s.beginTransaction();
+        String query = "FROM TMovimientoCuenta AS tmc "
+                + "WHERE tmc.tmocFecha BETWEEN  DATE(DATE_FORMAT('" + fechaIni + "', '%Y-%m-%d')) AND DATE(DATE_FORMAT('" + fechaFin + "', '%Y-%m-%d'))";;
+        List<TMovimientoCuenta> result = s.createQuery(query).list();
+        s.getTransaction().commit();
+
+        Iterator itr = result.iterator();
+        long valor = 0;
+
+        for (int i = 0; i < result.size(); i++) {
+            valor += result.get(i).getTmocSaldo();
+        }
+        return valor;
+    }
 }
